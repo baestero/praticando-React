b49bdf2dcb857f44981e0809d6d4a64eae3c4b75
baestero <leonardobaestero@gmail.com>
2025-06-24 12:44:35 -0300

    correção do exercicios routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./015 - Desafio Router - Meu desafio/Meu-Desafio-Router";
+import App from "./016 - Desafio Router - Correção/Correção - Desafio Router";
 
 createRoot(document.getElementById("root")).render(<App />);
