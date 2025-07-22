beb1505adf4fb0bec3d0ea343b06c7b8bd3d3d35
baestero <leonardobaestero@gmail.com>
2025-07-21 22:40:59 -0300

    aula validação

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./026 - Formulários Componentes/001-Formularios-Componentes";
+import App from ".//027 - Validação/001-Aula-Validação";
 
 createRoot(document.getElementById("root")).render(<App />);
